21cad7cad7544b3dcc153adb3f9a53bf56b7678b
caue-bueno <caue-bueno@live.com>
2023-08-13 13:32:49 -0300

    Finished authentication hook and signin page

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -3,8 +3,19 @@ import { FiMail, FiLock } from 'react-icons/fi'
 import { Input } from "../../components/Input"
 import { Button } from "../../components/Button"
 import { ButtonText } from "../../components/ButtonText"
+import { useAuth } from "../../hooks/auth"
+import { useState } from "react"
 
 export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  
+  const { signIn } = useAuth();
+  
+  function handleSignIn() {
+    signIn({ email, password });
+  }
+
   return (
     <Container>
       <Form>
@@ -13,9 +24,22 @@ export function SignIn() {
 
         <h3>Faça seu login</h3>
 
-        <Input icon={FiMail} placeholder="E-mail" type="text" />
-        <Input icon={FiLock} placeholder="Senha" type="password" />
-        <Button title="Entrar" />
+        <Input 
+        icon={FiMail} 
+        placeholder="E-mail" 
+        type="text"
+        onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <Input 
+        icon={FiLock} 
+        placeholder="Senha" 
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+        
+        />
+        
+        <Button title="Entrar" onClick={handleSignIn} />
 
         <ButtonText title="Criar conta" url="signup" />
 
