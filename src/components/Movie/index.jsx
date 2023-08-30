d256c875335e4b8c69f97b8a70ec0d745be4c668
caue-bueno <caue-bueno@live.com>
2023-08-30 08:07:51 -0300

    Finished movie component styles

--- src/components/Movie/index.jsx
+++ src/components/Movie/index.jsx
@@ -7,22 +7,27 @@ export function Movie({ data, ...rest }) {
   return (
     <Container {...rest}>
       <h2>{data.title}</h2>
-
-      {[...Array(5)].map((star, idx) => {
-        idx += 1;
-        return (
-          <React.Fragment key={idx}>
-            {idx <= data.rating ? <Star /> : <StarEmpty />}
-          </React.Fragment>
-        );
-      })}
+      <div>
+        {[...Array(5)].map((star, idx) => {
+          idx += 1;
+          return (
+            <React.Fragment key={idx}>
+              {idx <= data.rating ? <Star /> : <StarEmpty />}
+            </React.Fragment>
+          );
+        })}
+      </div>
 
       <p>{data.description}</p>
 
       {data.tags && (
-          data.tags.map((tag) => (
-            <Tag key={tag.name} title={tag.name} />
-          ))
+        <footer>
+          {
+            data.tags.map((tag) => (
+              <Tag key={tag.name} title={tag.name} />
+            ))
+          }
+        </footer>
       )}
 
     </Container>
--- src/components/Movie/styles.js
+++ src/components/Movie/styles.js
@@ -1,5 +1,40 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  
+  height: 22.3rem;
+  padding: 3.2rem;
+  margin-right: 8px;
+
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_2};
+
+  border-radius: 1.6rem;
+
+  > h2 {
+    margin-bottom: 0.8rem;
+  }
+
+  > p {
+    margin-bottom: 1.5rem;
+
+    color: ${({ theme }) => theme.COLORS.GRAY_700};
+
+    overflow: hidden;
+
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+  }
+
+  footer {
+    margin-top: 2rem;
+    display: flex;
+    gap: 1.5rem;
+  }
+
+    div {
+    display: flex;
+    gap: 0.6rem;
+    margin-bottom: 1.5rem;
+  }  
 `;
